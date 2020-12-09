4e2ca27f736703797e98967913441f598fe63bf0
peterhaaf <34441417+peterhaaf@users.noreply.github.com>
2020-12-09 12:31:21 +0100

    Update run.sh

--- homegear/script/run.sh
+++ homegear/script/run.sh
@@ -19,6 +19,7 @@ USER_GID=${HOST_USER_GID:=$USER_GID}
 
 sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:[0-9]*/${USER}:\1:${USER_ID}:${USER_GID}/"  /etc/passwd
 sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/" /etc/group
+touch /tmp/run-sh
 
 mkdir -p /config/homegear /share/homegear/lib /share/homegear/log
 chown $USER:$USER /config/homegear /share/homegear/lib /share/homegear/log
