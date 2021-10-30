37a519fc257caa20433e4010efc49a49c3ad08de
Peter Haaf <peter.haaf@sap.com>
2021-10-30 17:03:16 +0200

    python2 was missing

--- homegear/script/prerun.sh
+++ homegear/script/prerun.sh
@@ -18,5 +18,5 @@ rm -rf /etc/homegear /var/lib/homegear /var/log/homegear
 ln -nfs /config/homegear     /etc/homegear
 ln -nfs /share/homegear/lib /var/lib/homegear
 ln -nfs /share/homegear/log /var/log/homegear
-ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/python2 /usr/bin/python
 /start.sh
